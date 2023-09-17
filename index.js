b777a78105ccc98dc9ab7f1108a65f518d5872fa
Landon <landoncramer1@gmail.com>
2023-09-17 13:02:44 -0700

    complete method find-lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "A"},
+    //...
+  ]
+  function superbowlWin(arr) {
+    let winningYear = arr.find(checkWin);
+        if(winningYear) {
+        return winningYear.year;
+        } else {
+            return undefined;
+        }
+    }
+
+function checkWin(rec) {
+      if(rec.result === "W") {
+        return rec.year;  
+    }
+}
+
+ console.log(superbowlWin(record));
